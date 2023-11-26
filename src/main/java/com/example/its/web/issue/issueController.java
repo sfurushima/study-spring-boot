5c0cdb0ea53df80fc813271dfe1e075390439b56
sfurushima <sho.furushima.xyz999@gmail.com>
2023-11-26 18:45:36 +0900

    DI setted

--- src/main/java/com/example/its/web/issue/issueController.java
+++ src/main/java/com/example/its/web/issue/issueController.java
@@ -2,6 +2,7 @@ package com.example.its.web.issue;
 
 import com.example.its.domain.issue.IssueService;
 import com.example.its.domain.issue.issueEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
+// finalがついている ∧ 初期化されていないフィールドを初期化してくれる
+@RequiredArgsConstructor
 public class issueController {
 
-    private final IssueService issueService = new IssueService();
+    // @RequiredArgsConstructorで初期化してくれるため、new()が不要。
+    private final IssueService issueService;
     @GetMapping("/issues")
     public String showList(Model model) {
         // javaデータをThmeleafで表示するためには、SpringのMODELを利用して表示する必要がある
